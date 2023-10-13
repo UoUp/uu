4e1d85283f94842520b453689497af5951823b99
UoUp <147141429+UoUp@users.noreply.github.com>
2023-10-12 23:59:12 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="3c3c9e0f-ab00-464f-a326-c0322bbd2ac4"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
